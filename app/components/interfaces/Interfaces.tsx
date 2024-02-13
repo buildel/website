a61caa403a6b163104488c38eff39add2957c395
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-02-13 09:25:58 +0100

    Add accent to buildel in text

--- app/components/interfaces/Interfaces.tsx
+++ app/components/interfaces/Interfaces.tsx
@@ -6,9 +6,11 @@ interface InterfacesProps {}
 
 export const Interfaces: React.FC<InterfacesProps> = () => {
   return (
-    <section className="h-[70vh]">
+    <section>
       <header className="text-center">
-        <h2 className="text-3xl md:text-5xl mb-2">Try Buildel Interfaces</h2>
+        <h2 className="text-3xl md:text-5xl mb-2">
+          Try <span className="text-secondary-500">Buildel</span> Interfaces
+        </h2>
         <p className="text-sm md:text-base max-w-3xl mx-auto">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga
           pariatur repellendus. Ad asperiores beatae facilis id, illo molestiae
@@ -18,14 +20,16 @@ export const Interfaces: React.FC<InterfacesProps> = () => {
 
       <div className="mt-10 lg:mt-14">
         <TabGroup defaultActiveTab="chat">
-          <div className="w-full grid grid-cols-4 gap-2">
+          <div className="w-full grid grid-cols-4 gap-2 mb-8 lg:mb-10">
             <InterfaceTabButton tabId="chat">Chat</InterfaceTabButton>
             <InterfaceTabButton tabId="memory">Memory</InterfaceTabButton>
             <InterfaceTabButton tabId="providers">Providers</InterfaceTabButton>
             <InterfaceTabButton tabId="api-tools">Api Tools</InterfaceTabButton>
           </div>
 
-          <Tab tabId="chat">Chat1</Tab>
+          <Tab tabId="chat">
+            <ChatInterface />
+          </Tab>
           <Tab tabId="memory">memory</Tab>
           <Tab tabId="api-tools">api-tools</Tab>
         </TabGroup>
@@ -33,3 +37,17 @@ export const Interfaces: React.FC<InterfacesProps> = () => {
     </section>
   );
 };
+
+function ChatInterface() {
+  return (
+    <div className="flex flex-col gap-4 lg:flex-row">
+      <div className="w-full rounded-lg bg-zinc-900 border border-neutral-950 h-[30vh]">
+        <p>aa</p>
+      </div>
+
+      <div className="w-full rounded-lg bg-zinc-900 border border-neutral-950">
+        <p>aa</p>
+      </div>
+    </div>
+  );
+}
