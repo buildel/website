005bf5010daa47f4fd36e84a172d97517f053d65
Michal Warda <mw@grifel.dev>
2024-02-16 10:45:36 +0100

    Fix copy in workflows section

--- app/components/workflows/Workflows.tsx
+++ app/components/workflows/Workflows.tsx
@@ -9,21 +9,21 @@ import { Button } from "~/components/buttons/Button";
 const data = [
   {
     id: 0,
-    heading: "Access Buildel API easily with our client SDK.",
+    heading: "Access Buildel API easily with our client SDK",
     paragraph: `
 Buildel offers a straightforward and secure method for incorporating your Buildel Workflows into end-user web experiences. To ensure secure access to our applications directly from client-side code, we've integrated a Client SDK with an authentication mechanism.`,
     src: "/assets/client-sdk.webp",
   },
   {
     id: 1,
-    heading: "Integrate an workflow on your website with a embedded chatbot.",
-    paragraph: `It allows for a straightforward integration, offering your visitors the convenience of engaging with Buildel Chat directly on your site..`,
+    heading: "Embed Buildel Chat directly on Your Website",
+    paragraph: `It allows for a straightforward integration, offering your visitors the convenience of engaging with Buildel Chat directly on your site.`,
     src: "/assets/website-chatbot.webp",
   },
   {
     id: 2,
-    heading: "Connect to Our Custom API.",
-    paragraph: `Easily integrate your chatbot with our API by following these straightforward instructions.`,
+    heading: "Connect to Buildel through an OpenAI compliant interface",
+    paragraph: `Easily integrate your chatbot with our API by just changing the token and url in your app. Later if you want to change the chatbot, you can do it without changing the code.`,
     src: "/assets/open-ai.webp",
   },
 ];
@@ -33,11 +33,11 @@ export const Workflows: React.FC = () => {
     <section>
       <header className="text-center mb-10 lg:mb-14">
         <h2 className="text-3xl md:text-5xl mb-2">
-          Integrate Buildel to your app
+          Integrate Buildel into your app
         </h2>
         <p className="text-sm md:text-base max-w-3xl mx-auto">
-          Seamlessly Integrate Workflows to Elevate User Experience and
-          Streamline Operations.
+          Seamlessly Integrate Workflows to elevate User Experience and
+          Streamline Operations
         </p>
       </header>
 
