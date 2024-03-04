29cb87e8991b6873d0487dcb06336bc172ee5a0f
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2024-03-04 09:46:23 +0100

    animate client logos

--- app/components/sections/hero/Clients.tsx
+++ app/components/sections/hero/Clients.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const clients = [
   { name: "EL Passion", logo: "el-passion-logo.svg" },
   { name: "EL Passion", logo: "el-passion-logo.svg" },
@@ -11,16 +13,32 @@ export const Clients = () => (
     <h3 className="text-neutral-800 font-primaryBold w-full max-w-[400px] text-xl">
       Trusted by multiple clients from various industries with individual needs
     </h3>
-    <ul className="grid grid-cols-3 lg:flex lg:items-center mt-10 gap-11 lg:gap-x-14 w-full overflow-x-auto">
+    <motion.ul
+      variants={{
+        visible: {
+          transition: { staggerChildren: 0.1, bounce: 0, ease: "easeInOut" },
+        },
+      }}
+      initial="hidden"
+      animate="visible"
+      className="grid grid-cols-3 lg:flex lg:items-center mt-10 gap-11 lg:gap-x-14 w-full overflow-x-auto overflow-y-hidden"
+    >
       {clients.map((client, index) => (
-        <li key={index}>
+        <motion.li
+          key={`${client.name}-${index}`}
+          className="h-full"
+          variants={{
+            hidden: { y: 100 },
+            visible: { y: 0, transition: { bounce: 0, ease: "easeInOut" } },
+          }}
+        >
           <img
             src={`/assets/clients/${client.logo}`}
             className="opacity-20 hover:opacity-100 transition-opacity h-10"
             alt={client.name}
           />
-        </li>
+        </motion.li>
       ))}
-    </ul>
+    </motion.ul>
   </div>
 );
