f37ce7e5ece47f10421b608315c44fef10def427
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-11-22 15:04:12 +0100

    update post page

--- app/views/Blog/Post/Page.tsx
+++ app/views/Blog/Post/Page.tsx
@@ -2,14 +2,10 @@ import { useLoaderData } from "@remix-run/react";
 import { Footer } from "~/components/layout/Footer";
 import { Header } from "~/components/layout/Header";
 import { JoinCommunity } from "~/components/sections/community/JoinCommunity";
-import {
-  AnimatedWords,
-  HeroContainer,
-  HeroHeader,
-  HeroPreHeader,
-} from "~/components/sections/hero/Hero";
+
 import { loader } from "~/routes/blog_.$slug";
 import { posts } from "../BlogPostsData/blogPosts";
+import { Hero } from "~/views/Blog/Post/Hero";
 
 export const Page = () => {
   const data = useLoaderData<typeof loader>();
@@ -29,28 +25,10 @@ export const Page = () => {
   );
 };
 
-const Hero = ({
-  date,
-  title: { text, animatedText },
-}: {
-  date: string;
-  title: { text: string; animatedText?: string };
-}) => {
-  return (
-    <HeroContainer className="max-h-[30rem]">
-      <HeroPreHeader>{date}</HeroPreHeader>
-      <HeroHeader>
-        {`${text} `}
-        {animatedText && <AnimatedWords words={[animatedText, animatedText]} />}
-      </HeroHeader>
-    </HeroContainer>
-  );
-};
-
 const Post = ({ slug }: { slug: string }) => {
   return (
     <article
-      className="layout prose lg:prose-xl color-black prose-p:color-black pb-16"
+      className="layout prose lg:prose-xl color-black prose-p:color-black pb-16 pt-20"
       style={{ color: "var(--tw-prose-body)" }}
     >
       {posts.find((blogPostData) => blogPostData.slug === slug)?.article}
