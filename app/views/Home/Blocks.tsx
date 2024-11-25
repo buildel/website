f6f8c90cb8177101e5d1c22a69c2c0568be6b255
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-11-25 09:53:09 +0100

    fix title

--- app/views/Home/Blocks.tsx
+++ app/views/Home/Blocks.tsx
@@ -10,7 +10,7 @@ import {
 export function Blocks({ blocks }: BlockCarouselProps) {
   return (
     <SectionWrapper className="min-h-[220px] md:min-h-[300px] flex flex-col items-center justify-center gap-10 py-20 md:py-40">
-      <SectionHeading>
+      <SectionHeading className="text-center max-w-[440px]">
         Over <span className="gradient-text">{blocks.length} blocks </span>
         to fulfill your needs
       </SectionHeading>
